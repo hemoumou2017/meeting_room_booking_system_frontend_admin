16663e1f2075bd6b4d5427d072a197d9a56f8ab4
何欣 <1254409474@qq.com>
2024-10-14 15:13:01 +0800

    docker

--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-10-08 17:27:59
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-10-14 10:51:56
+ * @LastEditTime: 2024-10-14 15:00:50
  * @FilePath: /meeting_room_booking_system_frontend_admin/src/interfaces/interfaces.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,7 +16,7 @@ import { SearchBooking } from "../pages/BookingManage/BookingManage";
 import dayjs from "dayjs";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost/api',
+    baseURL: 'http://8.148.23.54/api',
     timeout: 3000
 });
 axiosInstance.interceptors.request.use(function (config) {
